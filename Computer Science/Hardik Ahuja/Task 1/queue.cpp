05866d7b619d4dd8acf70f826a002c2490eec497
Hardik Ahuja <hardikahuja9999@gmail.com>
2017-06-08 23:56:56 +0530

    Task 1

---
+++ Computer Science/Hardik Ahuja/Task 1/queue.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+class queue{
+	int first;
+	int last;
+	public:
+		int array[15];
+		
+		queue(){
+			first = -1;
+			last = -1;
+		}
+		
+		bool isfull(){
+			if(last>=15)
+				return true;
+			return false;
+		}
+
+		bool isempty(){
+			if((first==-1)&&(last==-1))
+				return true;
+			return false;
+		}
+
+		void push(int n){
+			if(!isfull()){
+				last++;
+				array[last] = n;
+			}
+			else
+				cout<<"Queue Full";
+		}
+
+		int pop(){
+			if(!isempty())
+				//return ++first;
+				return array[++first];
+			else
+				return 0;
+		}
+
+		void print(){
+			for(int i=last;i>first;i--){
+				cout<<array[i]<<"\n";
+			}
+		}
+};
+
+int main(){
+	queue s;
+	s.push(1);
+	s.push(4);
+	s.push(2);
+	s.push(6);
+	s.push(9);
+	s.push(12);
+	s.push(45);
+	s.push(23);
+	s.push(72);
+	s.push(32);
+	cout<<endl<<"pop"<<s.pop()<<"pop"<<endl; //pops out 1
+	s.push(s.pop()*10); //pops out 4 from end and add 40 at the start
+	s.print();
+}
---
+++ Computer Science/Hardik Ahuja/Task 1/readme.md
@@ -0,0 +1,9 @@
+# TASK-1
+## 1. Binary Tree
+   It includes **inserting** a value, **searching** a value and **printing** the binary tree.
+## 2. Linked List
+   It includes **adding** an element from front as well as end and **deleting** an element from linked list. It also includes  **reversing** a linked list.
+## 3. Stack
+   It includes **adding** of an element and **poping** an element from a stack
+## 4. Queue
+   It include **adding** of an element, **poping** it.
