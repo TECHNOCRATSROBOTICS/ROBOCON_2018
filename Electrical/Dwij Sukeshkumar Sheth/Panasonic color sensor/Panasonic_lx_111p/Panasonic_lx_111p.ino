f7ca1727e11e0e95f4ae17f3f4c39de40dbd107f
TECHNOCRATS ROBOTICS <technocratscse@gmail.com>
2017-08-31 21:27:24 +0530

    Add files via upload

---
+++ Electrical/Dwij Sukeshkumar Sheth/Panasonic color sensor/Panasonic_lx_111p/Panasonic_lx_111p.ino
@@ -0,0 +1,12 @@
+int out=8;
+
+void setup() {
+  pinMode(out,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //duration = pulseIn(echoPin, HIGH);
+  Serial.println(out);
+  delay(1000);
+}
---
+++ Electrical/Dwij Sukeshkumar Sheth/Ultrasonic_sensor/Ultrasonic_sensor.ino
@@ -0,0 +1,22 @@
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+int distance;
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*0.034/2;    //0.034 is the speed of sound in cm/micron seconds
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+}
