3efc08aaf84c7a11505dc6da472a391eb99fb497
TECHNOCRATS ROBOTICS <technocratscse@gmail.com>
2017-09-01 01:16:25 +0530

    Update MPU6050_calibration.ino
    The values that are returned after running the program are to be noted and are assumed to be the offset values of the sensor.

--- Electrical/Aylmer/IMU/MPU6050_calibration.ino
+++ Electrical/Aylmer/IMU/MPU6050_calibration.ino
@@ -1,4 +1,4 @@
-// Arduino sketch that returns calibration offsets for MPU6050 //   Version 1.1  (31th January 2014)
+ //a  Arduino sketch that returns calibration offsets for MPU6050 //   Version 1.1  (31th January 2014)
 // Done by Luis Ródenas <luisrodenaslorda@gmail.com>
 // Based on the I2Cdev library and previous work by Jeff Rowberg <jeff@rowberg.net>
 // Updates (of the library) should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
