3774abef5fa78aba7d1fb444e093880b904b1201
Dwij Sukeshkumar Sheth <dwijhariket@gmail.com>
2018-01-30 15:21:09 +0530

    Code for testing the pneumatic with Motor Driver

---
+++ Electrical/Dwij Sukeshkumar Sheth/Pneumatics/sketch_dec24c/sketch_dec24c.ino
@@ -0,0 +1,48 @@
+int pin1=5;
+int pin2=6;
+char stroke_state;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin1,OUTPUT);
+  pinMode(pin2,OUTPUT);
+  digitalWrite(pin1,HIGH);
+  digitalWrite(pin2,HIGH);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if(Serial.available())
+  {
+    Serial.println("Connection Established");
+  stroke_state=Serial.read();
+  Serial.println(stroke_state);
+  // put your main code here, to run repeatedly:
+if(stroke_state=='r')
+{
+digitalWrite(pin1,LOW);
+digitalWrite(pin2,HIGH);
+delay(1000);
+digitalWrite(pin1,LOW);
+digitalWrite(pin2,LOW);
+Serial.println("pin2 High");
+}
+else if(stroke_state=='l')
+{
+digitalWrite(pin1,HIGH);
+digitalWrite(pin2,LOW);
+delay(1000);
+digitalWrite(pin1,LOW);
+digitalWrite(pin2,LOW);
+Serial.println("pin1 LOW");
+}
+/*digitalWrite(pin1,LOW);
+digitalWrite(pin2,LOW);
+delay(1000);
+digitalWrite(pin1,HIGH);
+digitalWrite(pin2,LOW);*/
+  }
+
+
+}
